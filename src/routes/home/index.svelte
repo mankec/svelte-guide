478a4472ad9134faa67df5cb3f3100c67c42903f
mankec <milan.kecman270@gmail.com>
2022-06-29 20:57:21 +0200

    lazy commit

---
+++ src/routes/home/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Tweet from '$root/components/tweet.svelte'
+	import type { TweetType } from '$root/types'
+
+	export let tweets: TweetType[] = []
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>Feed</h1>
+
+{#each tweets as tweet (tweet.id)}
+	<Tweet {tweet} />
+{/each}
+
+<!-- ... -->
